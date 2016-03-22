8a827c0b9c706db56c3065c9c74251a6da87f100
alexlevy0 <alexlevy0@gmail.com>
2016-03-22 20:28:26 +0100

    test orientation device4

--- webgl-smoke/js/index.js
+++ webgl-smoke/js/index.js
@@ -153,7 +153,7 @@ function init() {
     }
     function deviceOrientationHandler(eGamma, eBeta, dir) {
         mouseX = eGamma;
-        mouseY = eBeta + 90;
+        mouseY = eBeta - 90;
     }
 
     document.body.appendChild( renderer.domElement );
